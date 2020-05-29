1a5860ebb8729060173b151c863de5a83584ad9c
Yaroslav Nosik <nosik.yaroslav@gmail.com>
2020-05-29 16:03:05 +0300

    Race condition fix

--- FeedStoreChallenge/In Memory Feature/InMemoryFeedStore.swift
+++ FeedStoreChallenge/In Memory Feature/InMemoryFeedStore.swift
@@ -16,26 +16,38 @@ public class InMemoryFeedStore: FeedStore {
     }
     
     private var cache: InMemoryCache?
+    private let queue = DispatchQueue(label: "InMemoryDispatchQueue", qos: .userInitiated, attributes: .concurrent)
     
     public init() { }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        if cache != nil {
-            cache = nil
+        queue.async(flags: .barrier) { [weak self] in
+            guard let self = self else { return }
+            
+            if self.cache != nil {
+                self.cache = nil
+            }
+            completion(nil)
         }
-        completion(nil)
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        cache = InMemoryCache(feed: feed, timestamp: timestamp)
-        completion(nil)
+        queue.async(flags: .barrier) { [weak self] in
+            guard let self = self else { return }
+            
+            self.cache = InMemoryCache(feed: feed, timestamp: timestamp)
+            completion(nil)
+        }
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        if let cache = cache {
-            completion(.found(feed: cache.feed, timestamp: cache.timestamp))
-        } else {
-            completion(.empty)
+        let cache = self.cache
+        queue.async {
+            if let cache = cache {
+                completion(.found(feed: cache.feed, timestamp: cache.timestamp))
+            } else {
+                completion(.empty)
+            }
         }
     }
 }
